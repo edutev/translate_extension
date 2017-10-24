3e5d4f5c7a04be1d2811eef75f9b17a64fa507d8
edutev <edutevelectronica@gmail.com>
2017-10-23 22:22:49 -0400

    Update translate_extension.js

--- translate_extension.js
+++ translate_extension.js
@@ -118,20 +118,7 @@
       }
     });
   }
-    $.ajax({
-    url: 'http://cdn.rawgit.com/edutev/traductor/master/get_lang.js',
-    dataType: 'jsonp',
-    success: function( lang ) {
-      defaultLang = lang;
-    },
-    error: function() {
-      console.log( "Unable to get default language" );
-      defaultLang = null;
-    },
-    complete: function() {
-      getYandexLanguages();
-    }
-  });
+   
   
   var descriptor = {
     blocks: [
